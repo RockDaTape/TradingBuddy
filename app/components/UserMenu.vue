1d9b6bf83e8780f91e2d1d0492847c120a3d9fdc
RockDaTape <purnell.j@me.com>
2025-06-19 12:20:34 +0100

    changed the dummy data to mine

--- app/components/UserMenu.vue
+++ app/components/UserMenu.vue
@@ -12,10 +12,10 @@ const colors = ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 
 const neutrals = ['slate', 'gray', 'zinc', 'neutral', 'stone']
 
 const user = ref({
-  name: 'Benjamin Canac',
+  name: 'Rocky Purnell',
   avatar: {
-    src: 'https://github.com/benjamincanac.png',
-    alt: 'Benjamin Canac'
+    src: '',
+    alt: 'Rocky Purnell'
   }
 })
 
--- app/pages/settings/index.vue
+++ app/pages/settings/index.vue
@@ -15,9 +15,9 @@ const profileSchema = z.object({
 type ProfileSchema = z.output<typeof profileSchema>
 
 const profile = reactive<Partial<ProfileSchema>>({
-  name: 'Benjamin Canac',
-  email: 'ben@nuxtlabs.com',
-  username: 'benjamincanac',
+  name: 'Rocky Purnell',
+  email: '',
+  username: 'RockDaTape',
   avatar: undefined,
   bio: undefined
 })
